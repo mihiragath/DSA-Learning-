a4b767c02816960b6d89597add3b1c58d798ac99
mihiragath <mihiragath8@gmail.com>
2024-09-04 01:21:12 +0530

    first commit

---
+++ for.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+ int main(){
+    int i,a,b,n;
+
+    cout<<"enter value"<<endl;
+   /* cin>>n;
+
+    i=1;
+
+    //for(i=1;i<n;i++);
+
+    for(; ;){
+        if(i<n){
+            cout<<"value:"<<i<<endl;
+          
+        }
+        else{
+            break;
+        }
+          i++;
+    }
+    */
+
+   for(a=0, b=1 ; a>1 && b>2 ; a--,b--){
+    cout<<a<<" "<<b<<endl;
+   }
+ }
