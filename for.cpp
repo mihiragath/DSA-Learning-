309ebb61dda55845479e9d26077f9be1ec1553b7
mihiragath <mihiragath8@gmail.com>
2024-09-05 00:11:27 +0530

    bitwise operator

--- for.cpp
+++ for.cpp
@@ -1,29 +1,36 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
- int main(){
-    int i,a,b,n;
+int main() {
+    int i, a, b, n; // Declare integer variables
 
-    cout<<"enter value"<<endl;
-   /* cin>>n;
+    cout << "Enter value: " << endl;
 
-    i=1;
+    /*
+    cin >> n; // Input the value for `n`
 
-    //for(i=1;i<n;i++);
+    i = 1; // Initialize `i` to 1
 
-    for(; ;){
-        if(i<n){
-            cout<<"value:"<<i<<endl;
-          
+    // This loop initializes `i` to 1 and intended to run until `i` is less than `n`,
+    // but the semicolon after the `for` makes it an empty loop:
+    // for (i = 1; i < n; i++);
+
+    // This infinite loop runs until `i` reaches `n`
+    for (; ; ) { 
+        if (i < n) { // Check if `i` is less than `n`
+            cout << "Value: " << i << endl; // Print the value of `i`
+        } else {
+            break; // Break the loop when `i` is no longer less than `n`
         }
-        else{
-            break;
-        }
-          i++;
+        i++; // Increment `i` by 1
     }
     */
 
-   for(a=0, b=1 ; a>1 && b>2 ; a--,b--){
-    cout<<a<<" "<<b<<endl;
-   }
- }
+    // This loop starts with `a = 0` and `b = 1`
+    // The condition `a > 1 && b > 2` is false initially, so this loop will not execute.
+    for (a = 0, b = 1; a > 1 && b > 2; a--, b--) {
+        cout << a << " " << b << endl; // This line would print `a` and `b` values if the loop runs
+    }
+
+    return 0; // Return 0 to indicate successful execution of the program
+}
--- prime.cpp
+++ prime.cpp
@@ -1,25 +1,27 @@
-#include <iostream> 
-
-using namespace std; 
+#include <iostream>
+using namespace std;
 
 int main() {
-    int n;
-    cout << "enter the value of n"<<endl;
-    cin>>n;
+    int n; // Declare an integer variable `n` to store the user's input
+    cout << "Enter the value of n: " << endl; // Prompt the user to enter a number
+    cin >> n; // Read the input value and store it in `n`
 
-    bool isprime =1;
+    bool isprime = 1; // Initialize a boolean variable `isprime` to true (1), assuming `n` is prime
 
-    for(int i=2;i<n;i++){
-        if(n%i==0){
-            isprime=0;
-            break;
+    // Loop to check if `n` has any divisors other than 1 and itself
+    for (int i = 2; i < n; i++) { // Start loop from 2 up to n-1
+        if (n % i == 0) { // Check if `n` is divisible by `i`
+            isprime = 0; // If `n` is divisible by `i`, set `isprime` to false (0)
+            break; // Exit the loop early since `n` is not a prime number
         }
     }
 
-    if(isprime==0){
-        cout<<"not a prime no"<<endl;
-    }else{
-        cout<<"is a prime no"<<endl;
+    // Check the value of `isprime` to determine if `n` is a prime number
+    if (isprime == 0) { // If `isprime` is false, then `n` is not a prime number
+        cout << "Not a prime number" << endl;
+    } else { // If `isprime` is true, then `n` is a prime number
+        cout << "Is a prime number" << endl;
     }
-    return 0;
+    
+    return 0; // Return 0 to indicate successful execution of the program
 }
